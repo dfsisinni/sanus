a41a55c43cfaa6d150728200f0de7fe0ee47cfce
sum-kcid <jayp1924@gmail.com>
2016-09-17 18:31:42 -0400

    added profile

--- client/scripts/patientDash.js
+++ client/scripts/patientDash.js
@@ -1,5 +1,11 @@
 Template.patientDash.helpers({
 	profileImageUrl: function () {
 		return '/resources/images/default-pic.jpg';
+	},
+	medicineCount: function() {
+		return !0? 'No': 0;
+	},
+	registrationDate: function() {
+		return moment().format('MMMM D, YYYY');
 	}
 });
