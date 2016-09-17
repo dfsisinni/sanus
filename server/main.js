7f031b5b3c286f07617ea9f83e2091f6eb38ddaf
Dane Sisinni <dfsisinni@gmail.com>
2016-09-17 01:03:09 -0400

    loading drugs into collection

--- server/main.js
+++ server/main.js
@@ -1,11 +1,24 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
 const Drugs = new Mongo.Collection('drugs');
 
 
 Meteor.startup(() => {
-  importDrugs();
-});
+  	var fs = require('fs');
+	var path = require('path');
+	var fileName = process.env.PWD + path.join(__dirname, 'drugs.json');
 
-var importDrugs = function () {
-	var fs = require('fs');
-}
+
+	fs.readFile(fileName, {encoding: 'utf-8'}, Meteor.bindEnvironment(function(err,data){
+		var obj = JSON.parse(data);
+		
+		Drugs.remove({});
+		for (var i = 0; i < obj.length; i++) {
+			Drugs.insert({
+				id: obj[i].id,
+				name: obj[i].name
+			});
+		}
+	}));
+});
