19822525021a82f79bb993fd4e651a73e8257296
Dane Sisinni <dfsisinni@gmail.com>
2016-09-17 08:09:58 -0400

    added publishing of webmd drug list - temp: in login events

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -33,5 +33,14 @@ Template.login.events({
 				toastr.error("Invalid login credentials!");
 			}
 		});
+
+
+		Meteor.call('getDrugList', 'tylenol', function (error, data) {
+			var content = data.content;
+			content = content.substr(content.indexOf('(') + 1, content.length - 1);
+			content = content.substr(0, content.lastIndexOf(')') -1);
+
+			console.log(JSON.parse(content).types[0].references);		
+		});
 	},
 });
--- server/main.js
+++ server/main.js
@@ -43,4 +43,11 @@ Meteor.methods({
 
 
 	},
+
+	'getDrugList' (query) {
+		console.log(query);
+		var result = Meteor.http.call("POSt", "http://www.webmd.com/api/qrl/LookupService.ashx?jsonp=jQuery18204622211846722426_1473889064854&format=json&metadata=rxotc%2Cgeneric_name%2Cmnid&q=" + query + "&s=3&sz=125&_=1473889942505");
+		return result;
+	},
+
 });
