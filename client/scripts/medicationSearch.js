871777040d8db8e16a3150f838533084bc997fb1
Dane Sisinni <dfsisinni@gmail.com>
2016-09-18 01:15:29 -0400

    added medication searching for patients

--- client/scripts/medicationSearch.js
+++ client/scripts/medicationSearch.js
@@ -1,5 +1,36 @@
+Template.medicationSearch.created = function () {
+	Session.set("searchResults", null);
+	Session.set("loading", false);
+};
+
 Template.medicationSearch.events({
 	'click .closeModal': function () {
 		Session.set('searchMedications', false);
+	},
+
+	'click .search-btn': function (event) {
+		var value = document.getElementById('searchMedications').value;
+		
+
+		Session.set("loading", true);
+		Meteor.call('getDrugList', value, function (error, data) {
+			var content = data.content;
+			content = content.substr(content.indexOf('(') + 1, content.length - 1);
+			content = content.substr(0, content.lastIndexOf(')') -1);
+
+			var ref = JSON.parse(content).types[0].references;
+			Session.set("loading", false);
+			Session.set("searchResults", ref);	
+		});
 	}
+});
+
+Template.medicationSearch.helpers({
+	searchResults: function () {
+		return Session.get("searchResults");
+	},
+
+	loading: function () {
+		return Session.get("lodaing");
+	},
 });
